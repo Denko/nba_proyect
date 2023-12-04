e57ecb48cd810779a4fd16412b87a112734c12a5
Alejandro Arco <alexarcosarria@gmail.com>
2023-12-04 20:12:49 +0000

    bases phase1_done_starting_stages

---
+++ models/staging/games_data/base_games_details.sql
@@ -0,0 +1,45 @@
+WITH games_details AS (
+    SELECT *
+    FROM {{ source('games_data','games_details') }}
+),
+
+renamed_casted AS (
+    SELECT
+        
+        _LINE, 
+        {{dbt_utils.generate_surrogate_key(['GAME_ID','PLAYER_ID'])}}::varchar(256) as GAMES_DATA_ID,
+        GAME_ID, 
+        TEAM_ID, 
+        TEAM_ABBREVIATION, 
+        TEAM_CITY, 
+        PLAYER_ID, 
+        PLAYER_NAME,
+        NICKNAME, 
+        START_POSITION, 
+        COMMENT, 
+        "MIN" AS MINUTES, 
+        FGM, 
+        FGA, 
+        FG_PCT, 
+        FG_3_M, 
+        FG_3_A, 
+        FG_3_PCT, 
+        FTM, 
+        FTA, 
+        FT_PCT, 
+        OREB, 
+        DREB, 
+        REB, 
+        AST, 
+        STL, 
+        BLK, 
+        "TO" AS TURNOVERS, 
+        PF, 
+        PTS, 
+        PLUS_MINUS, 
+        _FIVETRAN_SYNCED
+
+    FROM games_details
+)
+
+SELECT * FROM renamed_casted
---
+++ models/staging/games_data/base_teams.sql
@@ -0,0 +1,28 @@
+WITH teams AS (
+    SELECT *
+    FROM {{ source('games_data','teams') }}
+),
+
+renamed_casted AS (
+    SELECT
+        _LINE, 
+        LEAGUE_ID, 
+        TEAM_ID AS OLD_TEAM_ID,
+        {{dbt_utils.generate_surrogate_key(['TEAM_ID'])}}::varchar(256) as TEAM_ID, 
+        MIN_YEAR, 
+        MAX_YEAR, 
+        ABBREVIATION, 
+        NICKNAME, 
+        YEARFOUNDED, 
+        CITY, 
+        ARENA, 
+        ARENACAPACITY, 
+        OWNER, 
+        GENERALMANAGER, 
+        HEADCOACH, 
+        DLEAGUEAFFILIATION, 
+        _FIVETRAN_SYNCED
+    FROM teams
+)
+
+SELECT * FROM renamed_casted
